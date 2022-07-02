ca043ab2b1533cd10badb62fd22410bcc0eada9f
rajdyp <rajdyp@yahoo.co.in>
2022-07-02 16:57:04 -0700

    Update and rename agent_nodes.pp to docker_container.pp

---
+++ site/profile/manifests/docker_container.pp
@@ -0,0 +1,9 @@
+class profile::docker_container {
+  include 'docker'
+  docker::image { 'centos':
+    ensure    => 'present',
+  }
+  docker::run { 'dcw1.example.com':
+  image   => 'centos',
+  }
+}
