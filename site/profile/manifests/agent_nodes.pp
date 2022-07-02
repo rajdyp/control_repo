7a381137d75ec7e4d948bbf3ecf55f88efb66ece
rajdyp <rajdyp@yahoo.co.in>
2022-07-02 13:25:00 -0700

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -6,4 +6,12 @@ class profile::agent_nodes {
   dockeragent::node {
     'dockerdb.example.com':
   }
+  host {'dockerweb.example.com':
+    ensure => present,
+    ip     => '172.18.0.2',
+  }
+    host {'dockerdb.example.com':
+    ensure => present,
+    ip     => '172.18.0.3',
+  }
 }
