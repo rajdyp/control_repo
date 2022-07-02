841203645bb4cb61dd36e9d7d0b7e601376c2a30
rajdyp <rajdyp@yahoo.co.in>
2022-07-02 11:10:17 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -6,10 +6,10 @@ node 'puppetnode1.example.com' {
   include role::master_server
 }
 
-node \^dockerweb\ {
+node /^dockerweb/ {
   include role::app_server
 }
 
-node \^dockerdb\ {
+node /^dockerdb/ {
   include role::db_server
 }
