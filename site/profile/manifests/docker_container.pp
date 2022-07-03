9dd45c6af4e5c7827661fd6eb631108e0cea6255
rajdyp <rajdyp@yahoo.co.in>
2022-07-02 17:15:26 -0700

    Update docker_container.pp

--- site/profile/manifests/docker_container.pp
+++ site/profile/manifests/docker_container.pp
@@ -6,4 +6,10 @@ class profile::docker_container {
   docker::run { 'dcw1.example.com':
   image   => 'centos',
   }
+  docker::run { 'dockerweb.example.com':
+  ensure => absent,
+  }
+  docker::run { 'dockerdb.example.com':
+  ensure => absent,
+  }
 }
